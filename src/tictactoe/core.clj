f807b7627c7e92f210503af9841d8fe4977a8a0f
Emile <emile.swarts123@gmail.com>
2017-09-29 16:55:45 +0100

    WIP

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,24 +1,45 @@
 (ns tictactoe.core)
 
-(defn -main
-  "I don't do a whole lot."
-  []
-  (println "Hello, World!"))
-
 (defn game-board
-  ([board move mark]
-  (assoc board (- move 1) mark))
-  ([]
-  ["" "" "" "" "" "" "" "" ""]))
+  ([board move mark] (assoc board (- move 1) mark))
+  ([] ["" "" "" "" "" "" "" "" ""]))
+
+(def winning-indexes [
+                      [0 1 2]
+                      [3 4 5]
+                      [6 7 8]
+                      [0 4 8]
+                      [2 5 8]
+                      [0 3 6]
+                      [1 4 7]
+                      [2 4 6]
+                      ])
 
 (defn winning-board?
-  [board]
-    (and (= "X" (get board 0)) (= "X" (get board 1)) (= "X" (get board 2)))
-  )
+  [board current-player]
+  (some true? (map #(and
+                        (= current-player (get board (get %1 0)))
+                        (= current-player (get board (get %1 1)))
+                        (= current-player (get board (get %1 2)))) winning-indexes)))
 
 (defn game-progress
   [board]
   (cond
-    (every? clojure.string/blank? board) "not-over"
-    (winning-board? board) "X Wins"
+    (winning-board? board "X") "X Wins"
+    (winning-board? board "O") "O Wins"
+    (some clojure.string/blank? board) "not-over"
     :else "draw"))
+
+(defn cpu-move
+  [board current-player]
+  (first (map
+    #(
+      (cond
+        (winning-board?
+          (game-board board %1 current-player)
+          current-player)
+          :else %1)
+     [1 2 3 4 5 6 7 8 9]))
+  )
+
+(defn -main "Play the Game" [] (game-board))
--- test/tictactoe/core_test.clj
+++ test/tictactoe/core_test.clj
@@ -2,28 +2,45 @@
   (:require [clojure.test :refer :all]
             [tictactoe.core :refer :all]))
 
-(deftest emgty-game-state-test
+(deftest game
   (testing "Renders an empty grid"
     (is (= (game-board)
-           ["" "" "" "" "" "" "" "" ""]))))
+           ["" "" "" "" "" "" "" "" ""])))
 
-(deftest game-state-with-one-item
   (testing "Renders a grid with one item on it"
     (is (= (game-board ["" "" "" "" "" "" "" "" ""] 5 "X" )
-           ["" "" "" "" "X" "" "" "" ""]))))
+           ["" "" "" "" "X" "" "" "" ""])))
 
-(deftest game-state-with-two-items
  (testing "Renders a grid with two items on it"
    (is (= (game-board ["" "" "" "" "X" "" "" "" ""] 1 "O" )
-          ["O" "" "" "" "X" "" "" "" ""]))))
+          ["O" "" "" "" "X" "" "" "" ""])))
 
-
-; WinnerX, WinnerO, Draw, NotOver
-
-(deftest game-progress-test
   (testing "Not over with empty board"
     (is (= (game-progress ["" "" "" "" "" "" "" "" ""]) "not-over")))
+
   (testing "Draw with full board"
     (is (= (game-progress ["X" "O" "X" "O" "X" "O" "O" "X" "O"]) "draw")))
+
   (testing "Win for X"
-    (is (= (game-progress ["X" "X" "X" "O" "O" "" "" "" ""]) "X Wins"))))
+    (is (= (game-progress ["X" "X" "X" "O" "O" "" "" "" ""]) "X Wins")))
+
+  (testing "Win for X"
+    (is (= (game-progress ["O" "X" "O" "X" "X" "X" "" "" ""]) "X Wins")))
+
+  (testing "Win for X"
+    (is (= (game-progress ["O" "X" "O" "O" "X" "O" "X" "X" "X"]) "X Wins")))
+
+  (testing "Win for O"
+    (is (= (game-progress ["O" "O" "O" "X" "X" "" "" "" ""]) "O Wins")))
+
+  (testing "Win for O"
+    (is (= (game-progress ["X" "O" "X" "O" "X" "O" "O" "X" ""]) "not-over"))))
+
+(deftest cpu-move-test
+  (testing "Winning on the first square"
+   (is (= (cpu-move ["" "O" "O" "" "" "" "" "" ""] "O") 1 )))
+
+  (testing "Winning on the second square"
+   (is (= (cpu-move ["O" "" "O" "" "" "" "" "" ""] "O") 2 )))
+
+)
