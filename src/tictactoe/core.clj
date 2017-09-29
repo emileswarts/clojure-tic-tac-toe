4b904b0e02d485800a63122bfafa686f2e895e0d
Emile <emile.swarts123@gmail.com>
2017-09-29 14:45:37 +0100

    Add big-old-cheese

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -4,3 +4,9 @@
   "I don't do a whole lot."
   []
   (println "Hello, World!"))
+
+(defn big-old-cheese
+  ([board move mark]
+  (assoc board (- move 1) mark))
+  ([]
+  ["" "" "" "" "" "" "" "" ""]))
--- test/tictactoe/core_test.clj
+++ test/tictactoe/core_test.clj
@@ -4,4 +4,25 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest emgty-game-state-test
+  (testing "Renders an empty grid"
+    (is (= (big-old-cheese)
+           ["" "" "" "" "" "" "" "" ""]))))
+
+(deftest game-state-with-one-item
+  (testing "Renders a grid with one item on it"
+    (is (= (big-old-cheese ["" "" "" "" "" "" "" "" ""] 5 "X" )
+           ["" "" "" "" "X" "" "" "" ""]))))
+
+
+(deftest game-state-with-one-item
+ (testing "Renders a grid with two items on it"
+   (is (= (big-old-cheese ["" "" "" "" "X" "" "" "" ""] 1 "O" )
+          ["O" "" "" "" "X" "" "" "" ""]))))
+
+(deftest game-state-with-one-item
+  (testing "Renders a grid with one item on it"
+    (is (= (big-old-cheese ["" "" "" "" "" "" "" "" ""] 5 )
+           ["" "" "" "" "X" "" "" "" ""]))))
