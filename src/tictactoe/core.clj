b97a974101e189e47172d178eca463e90cdae869
Emile <emile.swarts123@gmail.com>
2017-10-01 18:32:42 +0100

    Inline functions

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -4,16 +4,7 @@
   ([board move mark] (assoc board (- move 1) mark))
   ([] ["" "" "" "" "" "" "" "" ""]))
 
-(def winning-indexes [
-                      [0 1 2]
-                      [3 4 5]
-                      [6 7 8]
-                      [0 4 8]
-                      [2 5 8]
-                      [0 3 6]
-                      [1 4 7]
-                      [2 4 6]
-                      ])
+(def winning-indexes [[0 1 2] [3 4 5] [6 7 8] [0 4 8] [2 5 8] [0 3 6] [1 4 7] [2 4 6] ])
 
 (defn winning-board?
   [board current-player]
@@ -32,14 +23,6 @@
 
 (defn cpu-move
   [board current-player]
-  (first (map
-    #(
-      (cond
-        (winning-board?
-          (game-board board %1 current-player)
-          current-player)
-          :else %1)
-     [1 2 3 4 5 6 7 8 9]))
-  )
+  (first (map #((cond (winning-board? (game-board board %1 current-player) current-player) :else %1) [1 2 3 4 5 6 7 8 9])))
 
 (defn -main "Play the Game" [] (game-board))
