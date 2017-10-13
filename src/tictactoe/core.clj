3c26ead32ba05a786d337eab7e9902cba22a2105
Richard Foster <richard@madetech.co.uk>
2017-10-13 12:22:32 +0100

    MLD 6/10/2017

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,5 +1,11 @@
 (ns tictactoe.core)
 
+(defn- opponent
+  [current-player]
+  (cond
+    "X" "O"
+    "O" "X"))
+
 (defn game-board
   ([board move mark] (assoc board (- move 1) mark))
   ([] ["" "" "" "" "" "" "" "" ""]))
@@ -12,8 +18,8 @@
                       [2 5 8]
                       [0 3 6]
                       [1 4 7]
-                      [2 4 6]
-                      ])
+                      [2 4 6]])
+
 
 (defn winning-board?
   [board current-player]
@@ -30,16 +36,38 @@
     (some clojure.string/blank? board) "not-over"
     :else "draw"))
 
+(defn valid-moves
+  [board]
+  (filter #(= (get board (- %1 1)) "")
+    [1 2 3 4 5 6 7 8 9]))
+
+(defn- weight-of-best-intermediate-move
+  [board player is-current-player weight]
+
+  (if is-current-player
+    (game-board board (cpu-move board player) player) (+ weight (weight-of-next-move board current-player))
+    (game-board board (cpu-move board player) player) (- weight (weight-of-next-move board current-player)))
+
+  (weight-of-best-intermediate-move board (opponent player) (not is-current-player)))
+
+(defn- weight-of-next-move
+  [board current-player]
+  #(cond
+    (winning-board?
+      (game-board board %1 current-player)
+      current-player)
+    2
+    (winning-board?
+      (game-board board %1 (opponent current-player))
+      (opponent current-player))
+    1
+    :else
+    0))
+
 (defn cpu-move
   [board current-player]
-  (first (map
-    #(
-      (cond
-        (winning-board?
-          (game-board board %1 current-player)
-          current-player)
-          :else %1)
-     [1 2 3 4 5 6 7 8 9]))
-  )
+  (apply max-key
+    (weight-of-next-move board current-player)
+    (valid-moves board)))
 
 (defn -main "Play the Game" [] (game-board))
--- test/tictactoe/core_test.clj
+++ test/tictactoe/core_test.clj
@@ -8,39 +8,62 @@
            ["" "" "" "" "" "" "" "" ""])))
 
   (testing "Renders a grid with one item on it"
-    (is (= (game-board ["" "" "" "" "" "" "" "" ""] 5 "X" )
+    (is (= (game-board ["" "" "" "" "" "" "" "" ""] 5 "X")
            ["" "" "" "" "X" "" "" "" ""])))
 
  (testing "Renders a grid with two items on it"
-   (is (= (game-board ["" "" "" "" "X" "" "" "" ""] 1 "O" )
+   (is (= (game-board ["" "" "" "" "X" "" "" "" ""] 1 "O")
           ["O" "" "" "" "X" "" "" "" ""])))
 
-  (testing "Not over with empty board"
-    (is (= (game-progress ["" "" "" "" "" "" "" "" ""]) "not-over")))
+ (testing "Not over with empty board"
+   (is (= (game-progress ["" "" "" "" "" "" "" "" ""]) "not-over")))
 
-  (testing "Draw with full board"
-    (is (= (game-progress ["X" "O" "X" "O" "X" "O" "O" "X" "O"]) "draw")))
+ (testing "Draw with full board"
+   (is (= (game-progress ["X" "O" "X" "O" "X" "O" "O" "X" "O"]) "draw")))
 
-  (testing "Win for X"
-    (is (= (game-progress ["X" "X" "X" "O" "O" "" "" "" ""]) "X Wins")))
+ (testing "Win for X"
+   (is (= (game-progress ["X" "X" "X" "O" "O" "" "" "" ""]) "X Wins")))
 
-  (testing "Win for X"
-    (is (= (game-progress ["O" "X" "O" "X" "X" "X" "" "" ""]) "X Wins")))
+ (testing "Win for X"
+   (is (= (game-progress ["O" "X" "O" "X" "X" "X" "" "" ""]) "X Wins")))
 
-  (testing "Win for X"
-    (is (= (game-progress ["O" "X" "O" "O" "X" "O" "X" "X" "X"]) "X Wins")))
+ (testing "Win for X"
+   (is (= (game-progress ["O" "X" "O" "O" "X" "O" "X" "X" "X"]) "X Wins")))
 
-  (testing "Win for O"
-    (is (= (game-progress ["O" "O" "O" "X" "X" "" "" "" ""]) "O Wins")))
+ (testing "Win for O"
+   (is (= (game-progress ["O" "O" "O" "X" "X" "" "" "" ""]) "O Wins")))
 
-  (testing "Win for O"
-    (is (= (game-progress ["X" "O" "X" "O" "X" "O" "O" "X" ""]) "not-over"))))
+ (testing "Win for O"
+   (is (= (game-progress ["X" "O" "X" "O" "X" "O" "O" "X" ""]) "not-over"))))
 
 (deftest cpu-move-test
   (testing "Winning on the first square"
-   (is (= (cpu-move ["" "O" "O" "" "" "" "" "" ""] "O") 1 )))
+    (is (= (cpu-move ["" "O" "O" "" "" "" "" "" ""] "O") 1)))
+
+  (testing "Preventing a loss on the first square"
+    (is (= (cpu-move ["" "O" "O" "" "" "" "" "" ""] "X") 1)))
 
   (testing "Winning on the second square"
-   (is (= (cpu-move ["O" "" "O" "" "" "" "" "" ""] "O") 2 )))
+    (is (= (cpu-move ["O" "" "O" "" "" "" "" "" ""] "O") 2)))
 
-)
+  (testing "Winning on the last square"
+    (is (= (cpu-move ["X" "O" "O" "O" "O" "O" "O" "O" ""] "O") 9)))
+
+  (testing "Winning when in danger of losing"
+    (is (= (cpu-move ["" "O" "O" "" "" "" "X" "X" ""] "X") 9)))
+
+  (testing "Winning when in danger of losing"
+    (is (= (cpu-move ["" "O" "O" "" "" "" "X" "X" ""] "X") 9)))
+
+  (testing "Setting up some great opportunities with a corner move"
+    (is (= (cpu-move ["X" "" "" "" "O" "" "" "" ""] "O") 3))))
+
+(deftest valid-moves-test
+  (testing "Valid moves on an empty board"
+    (is (= (valid-moves ["" "" "" "" "" "" "" "" ""]) [1 2 3 4 5 6 7 8 9])))
+
+  (testing "Valid moves on a half filled board"
+    (is (= (valid-moves ["O" "O" "" "X" "X" "" "" "O" ""]) [3 6 7 9])))
+
+  (testing "Valid moves on a full board"
+    (is (= (valid-moves ["O" "O" "X" "X" "X" "X" "X" "O" "X"]) []))))
